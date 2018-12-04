2ca122380eabe03f716c6c3f503a0e990e783e24
Matthias Schimek <matthias_schimek@gmx.de>
2018-12-04 18:56:53 +0100

    r script

--- forHLR_results/basic_script.r
+++ forHLR_results/basic_script.r
@@ -18,68 +18,149 @@ columns <- c("numberProcessors",
              "type",
              "time")
 
-#allData <- read.table("./output.txt", comment.char = "#", col.names = columns)
-allData <- read_table(file = "2018_12_02_H22_35/data.txt", col_types = 
-  cols(numberProcessors = col_integer(),
+colTypeSpec = cols(numberProcessors = col_integer(),
        samplePolicy = col_character(),
        iteration = col_integer(),
        size = col_double(),
        operation = col_character(),
        type = col_character(),
        time = col_double())
-, comment="-")
+
+# "Constants"
+POINTSIZE = 0.05
+
+#allData <- read.table("./output.txt", comment.char = "#", col.names = columns)
+allData <- read_table(file = "2018_12_02_H22_35/data.txt", col_types = colTypeSpec, comment="-")
 allData$numberProcessors <- as.factor(allData$numberProcessors)
 allDataWithoutIt1 <- filter(allData, iteration != 1)
 
 availableProcessorCounts <- unique(allData$numberProcessors)
 
+#functions
+scatterPlotOnOperation <- function(data_, operation_, pointSize, title) {
+  plot <- ggplot(data = filter(data_, operation == operation_)) 
+  plot <- plot + geom_point(mapping = aes(x = interaction(numberProcessors, type), y = time, colour = type),
+                            size = pointSize, position = "jitter")
+  plot <- plot + facet_wrap(~ samplePolicy)
+  plot <- plot + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+  plot <- plot + ggtitle(title)
+  return(plot)
+}
 
-#examine all-to-all-strings
+boxPlotOnOperation <- function(data_, operation_, title) {
+  plot <- ggplot(data = filter(data_, operation == operation_)) 
+  plot <- plot + geom_boxplot(mapping = aes(x = interaction(numberProcessors, type), y = time, colour = type)) 
+  plot <- plot + facet_wrap(~ samplePolicy)
+  plot <- plot + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+  plot <- plot + ggtitle(title) 
+  return(plot)
+}
 
-allToAllStrings <- filter(allDataWithoutIt1, operation == "all_to_all_strings")
+computeYLimitsBoxplot <- function(data_) {
+  return(boxplot.stats(data)$stats[c(1,5)])
+} 
 
-allToAllScatterPlot1000000 <- ggplot(data = filter(allToAllStrings, size == 1000000)) + 
-  geom_point(mapping = aes(x = interaction(numberProcessors, type), y = time, colour = type), position = "jitter") +
-  facet_wrap(~ samplePolicy) +
-  ggtitle("All-To-All Strings, StringSetSize = 1000000") + 
-  theme(axis.text.x = element_text(angle = 90, hjust = 1))
-
-allToAllBoxPlot1000000 <- ggplot(data = filter(allToAllStrings, size == 1000000)) + 
-  geom_boxplot(mapping = aes(x = interaction(numberProcessors, type), y = time, colour = type)) +
-  facet_wrap(~ samplePolicy) +
-  ggtitle("All-To-All Strings, StringSetSize = 1000000") + 
-  theme(axis.text.x = element_text(angle = 90, hjust = 1))
-
-allToAllScatterPlot5000000 <- ggplot(data = filter(allToAllStrings, size == 5000000)) + 
-  geom_point(mapping = aes(x = interaction(numberProcessors, type), y = time, colour = type), position = "jitter") +
-  facet_wrap(~ samplePolicy) +
-  ggtitle("All-To-All Strings, StringSetSize = 5000000") + 
-  theme(axis.text.x = element_text(angle = 90, hjust = 1))
-
-allToAllBoxPlot5000000 <- ggplot(data = filter(allToAllStrings, size == 5000000)) + 
-  geom_boxplot(mapping = aes(x = interaction(numberProcessors, type), y = time, colour = type)) +
-  facet_wrap(~ samplePolicy) +
-  ggtitle("All-To-All Strings, StringSetSize = 5000000") + 
-  theme(axis.text.x = element_text(angle = 90, hjust = 1))
-
-
+computeYLimitsBoxplot_v <- function(data_) {
+  min <- 999999999
+  max <- 0
+  for (name in colnames(data_)) {
+    yLimits <- computeYLimitsBoxplot(data_$name)
+    if (min > yLimits[0]) {
+      min <- yLimits[0]
+    }
+    if (max < yLimits[1]) {
+      max <- yLimits[1]
+    }
+  }
+  c(min, max)
+}
 
 
 
+boxPlotOnOperationProcsAreFacets <- function(data_, operation_, types_, title) {
+  filteredData <- filter(data_, operation == operation_, type %in% types_)
+  plot <- ggplot(data = filteredData) 
+  plot <- plot + geom_boxplot(mapping = aes(x = samplePolicy, y = time, colour = type)) 
+  plot <- plot + facet_wrap(~ numberProcessors, nrow = 1)
+  plot <- plot + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+  plot <- plot + ggtitle(title) 
+  return(plot)
+}
 
-plot1 <- ggplot(data=filter(allData, numberProcessors==4)) + 
-  geom_boxplot(mapping = aes(x=operation,y=time, color=type)) + 
-  facet_wrap(~ samplePolicy)
+boxPlotOnOperationProcsAreFacets_ <- function(data_, operation_, types_, title, ylimits) {
+  plot <- boxPlotOnOperationProcsAreFacets(data_, operation_, types_, title)
+  plot <- plot + coord_cartesian(ylim = ylimits)
+  return(plot)
+}
 
-plot2 <- ggplot(data=filter(allData, numberProcessors==128)) + 
-  geom_boxplot(mapping = aes(x=operation,y=time, color=type)) + 
-  facet_wrap(~ samplePolicy)
+scatterPlotOnOperationProcsOnXAxis <- function(data_, operation_, pointSize, title) {
+  plot <- ggplot(data = filter(data_, operation == operation_))
+  plot <- plot + geom_point(mapping = aes(x = numberProcessors, y = time, colour = type),
+                            size = pointSize, position = "jitter")
+  plot <- plot + facet_wrap(~ samplePolicy)
+  plot <- plot + ggtitle(title)
+  return(plot)
+}
+
+scatterPlotOnOperationProcsOnXAxisType <- function(data_, operation_, type_, pointSize, title) {
+  plot <- ggplot(data = filter(data_, operation == operation_, type %in% type_))
+  plot <- plot + geom_point(mapping = aes(x = numberProcessors, y = time, colour = type),
+                            size = pointSize, position = "jitter")
+  plot <- plot + facet_wrap(~ samplePolicy)
+  plot <- plot + ggtitle(title)
+  return(plot)
+}
+
+
+
 
 pdf(paste("plots.pdf",sep=""), width=10, height=5)
 
-allToAllScatterPlot1000000
-allToAllScatterPlot5000000
-allToAllBoxPlot1000000
-allToAllBoxPlot5000000
+smallStringSet <- filter(allDataWithoutIt1, size == 1000000)
+bigStringSet <- filter(allDataWithoutIt1, size == 5000000)
 
-dev.off()
+#All-to-All Strings
+operation = "all_to_all_strings"
+filterTime = c("avgTime", "maxTime", "minTime")
+filterLoss = c("avgLoss", "maxLoss", "minLoss")
+scatterPlotOnOperation(smallStringSet, operation, POINTSIZE, "All-to-All Strings, StringSetSize = 1000000")
+scatterPlotOnOperation(bigStringSet, operation, POINTSIZE, "All-to-All Strings, StringSetSize = 5000000")
+boxPlotOnOperation(smallStringSet, operation, "All-to-All Strings, StringSetSize = 1000000")
+boxPlotOnOperation(bigStringSet, operation, "All-to-All Strings, StringSetSize = 5000000")
+scatterPlotOnOperationProcsOnXAxis(smallStringSet, operation, POINTSIZE, "All-to-All Strings, StringSetSize = 1000000")
+scatterPlotOnOperationProcsOnXAxisType(smallStringSet, operation,
+                                       filterTime, POINTSIZE, "All-to-All Strings, Time only, StringSetSize = 1000000")
+scatterPlotOnOperationProcsOnXAxisType(smallStringSet, operation,
+                                       filterLoss, POINTSIZE, "All-to-All Strings, Loss only, StringSetSize = 1000000")
+boxPlotOnOperationProcsAreFacets(smallStringSet, operation, filterTime, "All-to-All Strings, StringSetSize = 1000000")
+boxPlotOnOperationProcsAreFacets_(filter(smallStringSet, numberProcessors != 32), operation, filterTime, "All-to-All Strings, ZoomIn, StringSetSize = 1000000", c(250000000, 420000000))
+#Sort String locally
+scatterPlotOnOperation(smallStringSet, "sort_locally", POINTSIZE, "Sort locally, StringSetSize = 1000000")
+scatterPlotOnOperation(bigStringSet, "sort_locally", POINTSIZE, "Sort locally, StringSetSize = 5000000")
+boxPlotOnOperation(smallStringSet, "sort_locally", "Sort Strings locally, StringSetSize = 1000000")
+boxPlotOnOperation(bigStringSet, "sort_locally", "Sort Strings locally, StringSetSize = 5000000")
+
+#Merge Sequences
+scatterPlotOnOperation(smallStringSet, "merge_ranges", POINTSIZE, "Merge Sequences, StringSetSize = 1000000")
+scatterPlotOnOperation(bigStringSet, "merge_ranges", POINTSIZE, "Merge Sequences, StringSetSize = 5000000")
+boxPlotOnOperation(smallStringSet, "merge_ranges", "Merge Sequences, StringSetSize = 1000000")
+boxPlotOnOperation(bigStringSet, "merge_ranges", "Merge Sequences, StringSetSize = 5000000")
+
+#Overall
+barPlot <- function(data_, operation_, type_, size_, title = " ") {
+  filteredData <- filter(data_, operation != operation_, type == type_, size == size_)
+  group <- group_by(filteredData, numberProcessors, samplePolicy, size, operation, type)
+  timeMean <- summarise(group, time = mean(time, rm.na = TRUE))
+  timeMean$size <- as.factor(timeMean$size)
+  plot <- ggplot(data = timeMean)
+  plot <- plot + geom_bar(mapping = aes(x = samplePolicy, y = time, fill = operation), stat="identity")
+  plot <- plot + facet_wrap(~ numberProcessors, labeller = label_both, nrow=1)
+  plot <- plot + ggtitle(title)
+
+  return(plot)
+}
+
+barPlot(allDataWithoutIt1, operation_ = "sorting_overall", type_ = "avgTime", size_ = 1000000, "AvgTime and StringSetSize = 1000000")
+barPlot(allDataWithoutIt1, operation_ = "sorting_overall", type = "avgTime", size = 5000000, "AvgTime and StringSetSize = 5000000")
+
+#plot1
