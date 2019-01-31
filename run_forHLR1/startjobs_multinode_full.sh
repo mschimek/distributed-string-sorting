5aef36ce7a991e559a283c609600ccbc123669b6
gw1960@partner.kit.edu <gw1960@fh1n988.localdomain>
2019-01-31 15:46:57 +0100

    changed start scripts for forHLR1

---
+++ run_forHLR1/startjobs_multinode_full.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+touch jobIdsMultinodeFull.txt
+rm jobIdsMultinodeFull.txt
+touch jobIdsMultinodeFull.txt
+
+sbatch --partition singlenode  --ntasks=2    --time=00:20:00 $1  >> jobIdsMultinodeFull.txt
+sbatch --partition singlenode  --ntasks=4    --time=00:20:00 $1  >> jobIdsMultinodeFull.txt
+sbatch --partition singlenode  --ntasks=8    --time=00:20:00 $1  >> jobIdsMultinodeFull.txt
+sbatch --partition singlenode  --ntasks=16   --time=00:20:00 $1  >> jobIdsMultinodeFull.txt
+sbatch --partition multinode   --ntasks=32   --time=00:20:00 $1  >> jobIdsMultinodeFull.txt
+sbatch --partition multinode   --ntasks=64   --time=00:20:00 $1  >> jobIdsMultinodeFull.txt
+sbatch --partition multinode   --ntasks=128  --time=00:20:00 $1  >> jobIdsMultinodeFull.txt
