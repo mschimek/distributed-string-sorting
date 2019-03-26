8909c94181f388d8b0bac3e5241b1d85220298a6
gw1960@partner.kit.edu <gw1960@fh1n988.localdomain>
2019-03-26 16:13:53 +0100

    change in scripts

--- run_forHLR1/startjobsFromFile.sh
+++ run_forHLR1/startjobsFromFile.sh
@@ -13,14 +13,14 @@ do
 	outputFile="./tmpScripts/${inputFile}_tmpScripts.sh"
 	echo "outputFile:  $outputFile"
 
-	sed s/DummyPath/"$completeInputFilePath"/ merge_sort_file.sh > $outputFile
+	sed s/DummyPath/"$completeInputFilePath"/ $1 > $outputFile
 
-	sbatch --partition singlenode  --ntasks=1    --time=02:25:00 $outputFile  >> $jobIds
-	sbatch --partition singlenode  --ntasks=2    --time=00:20:00 $1  >> $filename
-	sbatch --partition singlenode  --ntasks=4    --time=00:25:00 $1  >> $filename
-	sbatch --partition singlenode  --ntasks=8    --time=00:45:00 $1  >> $filename
-	sbatch --partition singlenode  --ntasks=16   --time=00:40:00 $1  >> $filename
-	sbatch --partition multinode   --ntasks=32   --time=00:45:00 $1  >> $filename
-	sbatch --partition multinode   --ntasks=64   --time=00:45:00 $1  >> $filename
-	sbatch --partition multinode   --ntasks=128  --time=00:45:00 $1  >> $filename
+	sbatch --partition singlenode  --ntasks=1    --time=02:25:00 $outputFile >> $jobIds
+	sbatch --partition singlenode  --ntasks=2    --time=00:20:00 $outputFile >> $jobIds
+	sbatch --partition singlenode  --ntasks=4    --time=00:25:00 $outputFile >> $jobIds
+	sbatch --partition singlenode  --ntasks=8    --time=00:45:00 $outputFile >> $jobIds
+	sbatch --partition singlenode  --ntasks=16   --time=00:40:00 $outputFile >> $jobIds
+	sbatch --partition multinode   --ntasks=32   --time=00:45:00 $outputFile >> $jobIds
+	sbatch --partition multinode   --ntasks=64   --time=00:45:00 $outputFile >> $jobIds
+	sbatch --partition multinode   --ntasks=128  --time=00:45:00 $outputFile >> $jobIds
 done
