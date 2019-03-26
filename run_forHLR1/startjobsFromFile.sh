593a8f2e81e924db0893c24b29b2c77e4e5b3407
gw1960@partner.kit.edu <gw1960@fh1n988.localdomain>
2019-03-26 15:13:27 +0100

    script

--- run_forHLR1/startjobsFromFile.sh
+++ run_forHLR1/startjobsFromFile.sh
@@ -1,29 +1,26 @@
 #!/bin/bash
 
+prefix="\.\.\/upload\/"
 
-
-prefix="../upload/"
-
-for file in $(cat $2)
+for inputFile in $(cat $2)
 do
-	echo file
-	filename="jobIds_$file.txt"
-        touch $filename 
-        rm $filename
-        touch $filename
-	completeFileName="$prefix$filename"
-	echo $completeFileName
+	jobIds="jobIds_${inputFile}.txt"
+        touch $jobIds
+        rm $jobIds
+        touch $jobIds
+	completeInputFilePath="$prefix$inputFile"
+	echo $completeInputFilePath
+	outputFile="./tmpScripts/${inputFile}_tmpScripts.sh"
+	echo "outputFile:  $outputFile"
 
-	sed s/$completeFileName/DummyPath > tmpScript
+	sed s/DummyPath/"$completeInputFilePath"/ merge_sort_file.sh > $outputFile
 
-	echo $(cat tmpScript)
-	sbatch --partition singlenode  --ntasks=1    --time=02:25:00 tmpScript  >> $filename
-	#sbatch --partition singlenode  --ntasks=2    --time=00:20:00 $1  >> $filename
-	#sbatch --partition singlenode  --ntasks=4    --time=00:25:00 $1  >> $filename
-	#sbatch --partition singlenode  --ntasks=8    --time=00:45:00 $1  >> $filename
-	#sbatch --partition singlenode  --ntasks=16   --time=00:40:00 $1  >> $filename
-	#sbatch --partition multinode   --ntasks=32   --time=00:45:00 $1  >> $filename
-	#sbatch --partition multinode   --ntasks=64   --time=00:45:00 $1  >> $filename
-	#sbatch --partition multinode   --ntasks=128  --time=00:45:00 $1  >> $filename
-	rm tmpScript
+	sbatch --partition singlenode  --ntasks=1    --time=02:25:00 $outputFile  >> $jobIds
+	sbatch --partition singlenode  --ntasks=2    --time=00:20:00 $1  >> $filename
+	sbatch --partition singlenode  --ntasks=4    --time=00:25:00 $1  >> $filename
+	sbatch --partition singlenode  --ntasks=8    --time=00:45:00 $1  >> $filename
+	sbatch --partition singlenode  --ntasks=16   --time=00:40:00 $1  >> $filename
+	sbatch --partition multinode   --ntasks=32   --time=00:45:00 $1  >> $filename
+	sbatch --partition multinode   --ntasks=64   --time=00:45:00 $1  >> $filename
+	sbatch --partition multinode   --ntasks=128  --time=00:45:00 $1  >> $filename
 done
