e844c80c745677c371e21d32dbe66ae5c867ea47
Matthias Schimek <matthias_schimek@gmx.de>
2019-04-08 14:42:44 +0200

    changed bug(?) in all reduce

--- src/mpi/allreduce.hpp
+++ src/mpi/allreduce.hpp
@@ -12,6 +12,7 @@
 #include <cmath>
 #include <type_traits>
 #include <vector>
+#include <algorithm>
 
 #include "mpi/environment.hpp"
 #include "mpi/type_mapper.hpp"
@@ -19,6 +20,17 @@
 #include "util/uint_types.hpp"
 
 namespace dss_schimek::mpi {
+
+
+template <typename DataType>
+inline std::vector<DataType> allgatherForAllReduce(
+    DataType& send_data, environment env = environment()) {
+    data_type_mapper<DataType> dtm;
+    std::vector<DataType> receive_data(env.size());
+    MPI_Allgather(&send_data, 1, dtm.get_mpi_type(), receive_data.data(), 1,
+        dtm.get_mpi_type(), env.communicator());
+    return receive_data;
+}
 
 static inline bool allreduce_and(
     bool& send_data, environment env = environment()) {
@@ -34,10 +46,8 @@ static inline DataType allreduce_max(
     DataType& send_data, environment env = environment()) {
     static_assert(std::is_arithmetic<DataType>(),
         "Only arithmetic types are allowed for allreduce_max.");
-    DataType receive_data;
-    MPI_Allreduce(&send_data, &receive_data, type_mapper<DataType>::factor(),
-        type_mapper<DataType>::type(), MPI_MAX, env.communicator());
-    return receive_data;
+    std::vector<DataType> elems = dss_schimek::mpi::allgatherForAllReduce(send_data);
+    return *std::max_element(elems.begin(), elems.end());
 }
 
 template <typename DataType>
@@ -45,83 +55,21 @@ static inline DataType allreduce_min(
     DataType& send_data, environment env = environment()) {
     static_assert(std::is_arithmetic<DataType>(),
         "Only arithmetic types are allowed for allreduce_min.");
-    DataType receive_data;
-    MPI_Allreduce(&send_data, &receive_data, type_mapper<DataType>::factor(),
-        type_mapper<DataType>::type(), MPI_MIN, env.communicator());
-    return receive_data;
+    auto elems = dss_schimek::mpi::allgatherForAllReduce(send_data);
+    return *std::min_element(elems.begin(), elems.end());
 }
 
 template <typename DataType>
 static inline DataType allreduce_sum(
-    const DataType& send_data, environment env = environment()) {
-    static_assert(std::is_arithmetic<DataType>(),
-        "Only arithmetic types are allowed for allreduce_sum.");
-    DataType receive_data;
-    MPI_Allreduce(&send_data, &receive_data, type_mapper<DataType>::factor(),
-        type_mapper<DataType>::type(), MPI_SUM, env.communicator());
-    return receive_data;
-}
-
-template <typename DataType>
-static inline std::vector<DataType> allreduce_sum(
-    std::vector<DataType>& send_data, environment env = environment()) {
-    static_assert(std::is_arithmetic<DataType>(),
-        "Only arithmetic types are allowed for allreduce_sum.");
-    std::vector<DataType> result(send_data.size());
-    MPI_Allreduce(send_data.data(), result.data(), send_data.size(),
-        type_mapper<DataType>::type(), MPI_SUM, env.communicator());
-    return result;
-}
-
-template <typename DataType, size_t Length>
-static inline std::array<DataType, Length> allreduce_sum(
-    std::array<DataType, Length>& send_data, environment env = environment()) {
-    static_assert(std::is_arithmetic<DataType>(),
-        "Only arithmetic types are allowed for allreduce_sum.");
-    std::array<DataType, Length> result;
-    MPI_Allreduce(send_data.data(), result.data(),
-        Length * type_mapper<DataType>::factor(), type_mapper<DataType>::type(),
-        MPI_SUM, env.communicator());
-    return result;
-}
-
-template <size_t Length>
-static inline std::array<dsss::uint40, Length> allreduce_sum(
-    std::array<dsss::uint40, Length>& send_data,
-    environment env = environment()) {
-
-    std::array<size_t, Length> tmp;
-    for (size_t i = 0; i < Length; ++i) {
-        tmp[i] = send_data[i];
-    }
-    tmp = allreduce_sum(tmp, env);
-    std::array<dsss::uint40, Length> result;
-    for (size_t i = 0; i < Length; ++i) {
-        result[i] = tmp[i];
-    }
-    return result;
-}
-
-template <typename DataType>
-static inline DataType allreduce_avg(
     DataType& send_data, environment env = environment()) {
     static_assert(std::is_arithmetic<DataType>(),
-        "Only arithmetic types are allowed for allreduce_avg.");
-    DataType receive_data = allreduce_sum(send_data);
-    return receive_data / env.size();
+        "Only arithmetic types are allowed for allreduce_sum.");
+
+    auto elems = dss_schimek::mpi::allgatherForAllReduce(send_data);
+    return std::accumulate(elems.begin(), elems.end(), 0ull);
 }
 
-// Sample standard deviation
-template <typename DataType>
-static inline DataType allreduce_ssd(
-    DataType& send_data, environment env = environment()) {
-    static_assert(std::is_arithmetic<DataType>(),
-        "Only arithmetic types are allowed for allreduce_ssd.");
-    DataType receive_data = allreduce_avg(send_data);
-    DataType tmp = std::pow(send_data - receive_data, 2);
-    receive_data = allreduce_sum(tmp);
-    return std::sqrt(receive_data / env.size());
-}
+
 
 } // namespace dss_schimek::mpi
 
