107edce404f27acac5eee6fad4d5ea66a92aef99
Matthias Schimek <matthias_schimek@gmx.de>
2019-06-06 18:23:59 +0200

    added title to evalutation overall

--- forHLR_results/overall.r
+++ forHLR_results/overall.r
@@ -11,7 +11,6 @@ if (length(args) < 1) {
 }
 
 colTypeSpec = cols(numberProcessors = col_integer(),
-       samplePolicy = col_character(),
        iteration = col_integer(),
        size = col_double(),
        operation = col_character(),
@@ -22,12 +21,15 @@ colTypeSpec = cols(numberProcessors = col_integer(),
 
 pathToJSON = args[[1]]
 pdfTitle = args[[2]]
-jsonObject <- read_json(pathToJSON, simplifyDataFrame=TRUE)
+jsonMetaObject <- read_json(pathToJSON, simplifyDataFrame=TRUE)
+jsonObject <- jsonMetaObject["data"][[1]]
+title <- jsonMetaObject["title"]
+isD2N <- TRUE == (jsonMetaObject["isDToN"])
 print(str(jsonObject))
 length <- length(jsonObject)
 data <- vector("list", length)
 filters <- vector("list", length)
-print(length)
+print(paste("number datasets: ", length))
 for (i in c(1:length)){
   print(i)
   curPath = jsonObject[[i]]$path
@@ -37,9 +39,16 @@ for (i in c(1:length)){
   filters[[i]] <- jsonObject[[i]]$filter
   print(filters[[i]])
 }
+numberExpandedDatasets <- function() {
+  counter <- 0
+  for (i in c(1:length(datasets))) {
+    numberFilters <- nrow(filters[[i]][[1]])
+    counter <- counter + numberFilters
+  }
+  return(counter)
+}
 
 barPlotWhitelist <- function(datasets, operation_, type_ = "maxTime", title = " ", work = FALSE) {
-  isD2N <- TRUE
   set <- "dummy"
   print(paste("length data set: ", length(datasets)))
   counter <- 0
@@ -58,41 +67,47 @@ barPlotWhitelist <- function(datasets, operation_, type_ = "maxTime", title = " 
 
       df = filters[[i]][[1]]
       names <- colnames(df)
+      #print(data[[i]])
       localSets[[counter]] <- filter(data[[i]], operation == operation_, type == type_)
-      for (j in c(2:length(names))){
-        localSets[[counter]] <- filter(localSets[[counter]], UQ(as.name(names[j])) == df[k,j])
+      #print(localSets[[counter]])
+      if (length(names) > 1) {
+        for (j in c(2:length(names))){
+          localSets[[counter]] <- filter(localSets[[counter]], UQ(as.name(names[j])) == df[k,j])
+        }
       }
+      #print(localSets[[counter]])
       localSets[[counter]] <- mutate(localSets[[counter]], name = df[k, 1])
-      if (i == 1)
+      localSets[[counter]] <- select(localSets[[counter]], numberProcessors, dToNRatio,  operation, type, name, iteration, value)
+      if (k == 1 && i == 1){
         set <- localSets[[counter]]
+      }
       else {
         set <- rbind(set, localSets[[counter]])
       }
     }
     counter <- counter + 1
-   
-
     }
     set$numberProcessors <- as.factor(set$numberProcessors)
-    groupByIterations <- group_by(set, numberProcessors, dToNRatio, samplePolicy, ByteEncoder, size, operation, type, name)
+    groupByIterations <- group_by(set, numberProcessors, dToNRatio,  operation, type, name)
     valueMean <- summarise(groupByIterations, value = mean(value, rm.na = TRUE))
     valueMean$value <- valueMean$value / 10^9
-  plot <- ggplot(data = valueMean, mapping = aes(x = numberProcessors, y = value, group = name, colour = name, shape = name))
+  plot <- ggplot(data = valueMean, mapping = aes(x = numberProcessors, y = value, group = name, colour = name, shape = name, linetype = name))
   #plot <- plot + geom_bar(mapping = aes(x = numberProcessors, y = value, fill = name), stat="identity", position="dodge", colour="black", width=0.5)
   plot <- plot + ylab("time [sec]")
   plot <- plot + xlab("PEs")
   plot <- plot + theme_light()
   plot <- plot + geom_point()
   plot <- plot + geom_line()
+  plot <- plot + scale_y_continuous(trans='log2')
   plot <- plot + theme(panel.spacing = unit(1.25, "lines"))
+  plot <- plot + theme(plot.title = element_text(hjust = 0.5))
   if (isD2N) {
   plot <- plot + facet_wrap(~dToNRatio, labeller = label_both, nrow=1)
-  } else {
-  plot <- plot + facet_wrap(samplePolicy ~ ByteEncoder, labeller = label_both, nrow=1)
-  }
+  } 
+  plot <- plot + ggtitle(title)
   return (plot)
 
 }
 
 pdf(paste("evaluation/", pdfTitle, ".pdf",sep=""), width=10, height=5)
-barPlotWhitelist(c(1,2), "sorting_overall")
+barPlotWhitelist(c(1:length(data)), "sorting_overall", "maxTime", title)
