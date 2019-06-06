8e673f1bd99c1c479f6f50cd3326952abdd991c4
Matthias Schimek <matthias_schimek@gmx.de>
2019-06-07 00:35:23 +0200

    evalutation

---
+++ forHLR_results/evaluate.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+Rscript overall.r jsonDToN DToN
+Rscript overall.r jsonCommonCrawl CommonCrawl
+Rscript overall.r jsonWiki Wiki
+
--- forHLR_results/overall.r
+++ forHLR_results/overall.r
@@ -2,6 +2,7 @@ library(ggplot2)
 library(tidyverse)
 library(magrittr)
 library(jsonlite)
+library(latex2exp)
 
 args = commandArgs(trailingOnly=TRUE)
 
@@ -48,7 +49,7 @@ numberExpandedDatasets <- function() {
   return(counter)
 }
 
-barPlotWhitelist <- function(datasets, operation_, type_ = "maxTime", title = " ", work = FALSE) {
+lineplot <- function(datasets, operation_, type_ = "maxTime", title = " ", work = FALSE) {
   set <- "dummy"
   print(paste("length data set: ", length(datasets)))
   counter <- 0
@@ -88,19 +89,23 @@ barPlotWhitelist <- function(datasets, operation_, type_ = "maxTime", title = " 
     counter <- counter + 1
     }
     set$numberProcessors <- as.factor(set$numberProcessors)
+    set$value <- set$value / 10^9
     groupByIterations <- group_by(set, numberProcessors, dToNRatio,  operation, type, name)
-    valueMean <- summarise(groupByIterations, value = mean(value, rm.na = TRUE))
-    valueMean$value <- valueMean$value / 10^9
+    valueMean <- summarise(groupByIterations, sd = sd(value), value = mean(value, rm.na = TRUE))
   plot <- ggplot(data = valueMean, mapping = aes(x = numberProcessors, y = value, group = name, colour = name, shape = name, linetype = name))
-  #plot <- plot + geom_bar(mapping = aes(x = numberProcessors, y = value, fill = name), stat="identity", position="dodge", colour="black", width=0.5)
   plot <- plot + ylab("time [sec]")
   plot <- plot + xlab("PEs")
   plot <- plot + theme_light()
-  plot <- plot + geom_point()
-  plot <- plot + geom_line()
+  plot <- plot + geom_point(position=position_dodge(width=0.1))
+  plot <- plot + geom_line(position=position_dodge(width=0.1))
+  #plot <- plot + geom_errorbar(aes(ymin=value-sd, ymax=value+sd), width=.2,
+  #                 position=position_dodge(.1))
+
   plot <- plot + scale_y_continuous(trans='log2')
   plot <- plot + theme(panel.spacing = unit(1.25, "lines"))
   plot <- plot + theme(plot.title = element_text(hjust = 0.5))
+  plot <- plot + theme(strip.background =element_rect(fill="white"))
+  plot <- plot + theme(strip.text = element_text(colour = 'black'))
   if (isD2N) {
   plot <- plot + facet_wrap(~dToNRatio, labeller = label_both, nrow=1)
   } 
@@ -109,5 +114,80 @@ barPlotWhitelist <- function(datasets, operation_, type_ = "maxTime", title = " 
 
 }
 
+stackedBarPlot <- function(datasets, dToNRatio_, operations_, type_ = "maxTime", title = " ", work = FALSE) {
+  set <- "dummy"
+  print(paste("length data set: ", length(datasets)))
+  counter <- 0
+  for (i in c(1:length(datasets))) {
+    numberFilters <- nrow(filters[[i]][[1]])
+    counter <- counter + numberFilters
+  }
+  print(paste("counter: ", counter))
+  localSets <- vector("list", counter)
+  counter <- 1
+  for (i in c(1:length(datasets))) {
+    j <- ncol(filters[[i]][[1]]) 
+    numberFilters <- nrow(filters[[i]][[1]])
+    print(paste("numberFilters: ", numberFilters))
+    for (k in c(1:numberFilters)){
+
+      df = filters[[i]][[1]]
+      names <- colnames(df)
+      #print(data[[i]])
+      if (isD2N) {
+        localSets[[counter]] <- filter(data[[i]], operation %in% operations_, type == type_, dToNRatio == dToNRatio_)
+      } else {
+        localSets[[counter]] <- filter(data[[i]], operation %in% operations_, type == type_)
+      }
+      #print(localSets[[counter]])
+      if (length(names) > 1) {
+        for (j in c(2:length(names))){
+          localSets[[counter]] <- filter(localSets[[counter]], UQ(as.name(names[j])) == df[k,j])
+        }
+      }
+      #print(localSets[[counter]])
+      localSets[[counter]] <- mutate(localSets[[counter]], name = df[k, 1])
+      localSets[[counter]] <- select(localSets[[counter]], numberProcessors, dToNRatio,  operation, type, name, iteration, value)
+      if (k == 1 && i == 1){
+        set <- localSets[[counter]]
+      }
+      else {
+        set <- rbind(set, localSets[[counter]])
+      }
+    }
+    counter <- counter + 1
+    }
+    set$numberProcessors <- as.factor(set$numberProcessors)
+    set$value <- set$value / 10^9
+    groupByIterations <- group_by(set, numberProcessors, dToNRatio,  operation, type, name)
+    valueMean <- summarise(groupByIterations , value = mean(value, rm.na = TRUE))
+  plot <- ggplot(data = valueMean, mapping = aes(x = name, y = value, fill=operation))
+  plot <- plot + ylab("time [sec]")
+  plot <- plot + xlab("")
+  plot <- plot + theme_light()
+  plot <- plot + geom_bar(stat="identity")
+
+  plot <- plot + theme(panel.spacing = unit(1.25, "lines"))
+  plot <- plot + theme(plot.title = element_text(hjust = 0.5))
+  plot <- plot + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+  plot <- plot + theme(strip.background =element_rect(fill="white"))
+  plot <- plot + theme(strip.text = element_text(colour = 'black'))
+  plot <- plot + theme(plot.subtitle=element_text(hjust=0.5, size=12))
+  plot <- plot + facet_wrap(~numberProcessors, labeller = label_value, nrow=1) 
+  plot <- plot + ggtitle(title)
+  plot <- plot + labs(subtitle = "number of PEs")
+  return (plot)
+
+}
+
 pdf(paste("evaluation/", pdfTitle, ".pdf",sep=""), width=10, height=5)
-barPlotWhitelist(c(1:length(data)), "sorting_overall", "maxTime", title)
+lineplot(c(1:length(data)), "sorting_overall", "maxTime", title)
+operations = c("sort_splitter","prefix_decompression", "merge_ranges", "compute_ranges", "all_to_all_strings", "compute_interval_sizes", "choose_splitters", "allgather_splitters", "sample_splitters", "sort_locally", "bloomfilter_overall")
+if (isD2N) {
+  for (d in unique(data[[i]]$dToNRatio)) {
+    print(stackedBarPlot(c(1:length(data)), dToNRatio_ = d, operations_ = operations, "maxTime", paste(title, " with D/N-ratio: ", d)))
+  }
+} else {
+  print(stackedBarPlot(c(1:length(data)), dToNRatio_ = 0.5, operations_ = operations, "maxTime", title))
+}
+
