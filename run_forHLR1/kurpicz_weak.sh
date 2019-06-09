3e72b24fd12f111494a5d5af3e0a8aa0393cce1d
gw1960@partner.kit.edu <gw1960@fh1n989.localdomain>
2019-06-09 20:37:22 +0200

    analyse

---
+++ run_forHLR1/kurpicz_weak.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#module load mpi/openmpi/3.1
+module load mpi/impi/2019
+#export I_MPI_HYDRA_BRANCH_COUNT=-1
+
+executable="/home/fh1-project-kalb/gw1960/kurpiczModified/build/benchmark/dss"
+numOfStrings=500000
+numOfIterations=6
+generator=1
+stringLength=500
+
+for dToNRatio in 0.0 0.25 0.5 0.75 1.0
+do
+		#mpirun --mca coll_tuned_use_dynamic_rules 1 --mca coll_tuned_allgatherv_algorithm 1 --bind-to core --map-by core -report-bindings $executable --size $numOfStrings --numberOfIterations $numOfIterations --byteEncoder $byteEncoder --generator $generator --dToNRatio $dToNRatio --stringLength $stringLength --strongScaling
+                mpiexec.hydra -bootstrap slurm  $executable --size $numOfStrings -i $numOfIterations --dToNRatio $dToNRatio --stringLength $stringLength --generator $generator
+		#mpirun --bind-to core --map-by core  $executable --size $numOfStrings --numberOfIterations $numOfIterations --byteEncoder $byteEncoder --generator $generator --dToNRatio $dToNRatio --stringLength $stringLength --strongScaling  --sampleStringsPolicy $sampler --MPIRoutineAllToAll $MPIRoutine --compressLcps
+done
+#mpirun -np 2 $executable --size $numOfStrings --numberOfIterations $numOfIterations --byteEncoder $byteEncoder --generator $generator --dToNRatio $dToNRatio --stringLength $stringLength
+#
+#dToNRatio=0.4
+#mpirun -np 2 $executable --size $numOfStrings --numberOfIterations $numOfIterations --byteEncoder $byteEncoder --generator $generator --dToNRatio $dToNRatio --stringLength $stringLength
+#
+#dToNRatio=0.8
+#mpirun -np 2 $executable --size $numOfStrings --numberOfIterations $numOfIterations --byteEncoder $byteEncoder --generator $generator --dToNRatio $dToNRatio --stringLength $stringLength
+#
+#dToNRatio=1.0
+#mpirun -np 2 $executable --size $numOfStrings --numberOfIterations $numOfIterations --byteEncoder $byteEncoder --generator $generator --dToNRatio $dToNRatio --stringLength $stringLength
+#
+#byteEncoder=1
+#dToNRatio=0.2
+#mpirun -np 2 $executable --size $numOfStrings --numberOfIterations $numOfIterations --byteEncoder $byteEncoder --generator $generator --dToNRatio $dToNRatio --stringLength $stringLength
+#
+#dToNRatio=0.4
+#mpirun -np 2 $executable --size $numOfStrings --numberOfIterations $numOfIterations --byteEncoder $byteEncoder --generator $generator --dToNRatio $dToNRatio --stringLength $stringLength
+#
+#dToNRatio=0.8
+#mpirun -np 2 $executable --size $numOfStrings --numberOfIterations $numOfIterations --byteEncoder $byteEncoder --generator $generator --dToNRatio $dToNRatio --stringLength $stringLength
+#
+#dToNRatio=1.0
+#mpirun -np 2 $executable --size $numOfStrings --numberOfIterations $numOfIterations --byteEncoder $byteEncoder --generator $generator --dToNRatio $dToNRatio --stringLength $stringLength
+
+
---
+++ run_forHLR1/start_weak.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+
+
+touch jobIdsMultinodeFull.txt
+rm jobIdsMultinodeFull.txt
+touch jobIdsMultinodeFull.txt
+
+#sbatch --partition singlenode  --ntasks=1    --time=01:25:00 $1  >> jobIdsMultinodeFull.txt
+#sbatch --partition singlenode  --ntasks=2    --time=03:50:00 $1  >> jobIdsMultinodeFull.txt
+#sbatch --partition singlenode  --ntasks=4    --time=03:55:00 $1  >> jobIdsMultinodeFull.txt
+#sbatch --partition singlenode  --ntasks=10    --time=01:45:00 $1  >> jobIdsMultinodeFull.txt
+#sbatch --partition develop  --ntasks=20   --time=00:35:00 $1  >> jobIdsMultinodeFull.txt
+#sbatch --partition multinode   --ntasks=40   --time=00:05:00 $1  >> jobIdsMultinodeFull.txt
+#sbatch --partition multinode   --ntasks=80   --time=00:05:00 $1  >> jobIdsMultinodeFull.txt
+#sbatch --partition multinode   --ntasks=160  --time=00:05:00 $1  >> jobIdsMultinodeFull.txt
+#sbatch --partition multinode   --ntasks=320  --time=00:05:00 $1  >> jobIdsMultinodeFull.txt
+#sbatch --partition multinode   --ntasks=640  --time=00:05:00 $1  >> jobIdsMultinodeFull.txt
+#sbatch --partition multinode   --ntasks=1280  --time=00:40:00 $1  >> jobIdsMultinodeFull.txt
+
+#batch --partition singlenode  --ntasks=1    --time=00:05:00 $1  >> jobIdsMultinodeFull.txt
+#sbatch --partition develop  --ntasks=2    --time=00:05:00 $1  >> jobIdsMultinodeFull.txt
+#sbatch --partition singlenode  --ntasks=4    --time=00:05:00 $1  >> jobIdsMultinodeFull.txt
+#sbatch --partition singlenode  --ntasks=8    --time=00:05:00 $1  >> jobIdsMultinodeFull.txt
+sbatch --partition singlenode  --ntasks=20   --time=01:35:00 $1  >> jobIdsMultinodeFull.txt
+sbatch --partition multinode   --ntasks=40   --time=01:35:00 $1  >> jobIdsMultinodeFull.txt
+sbatch --partition multinode   --ntasks=80   --time=02:15:00 $1  >> jobIdsMultinodeFull.txt
+sbatch --partition multinode   --ntasks=160  --time=02:25:00 $1  >> jobIdsMultinodeFull.txt
+sbatch --partition multinode   --ntasks=320  --time=02:15:00 $1  >> jobIdsMultinodeFull.txt
+sbatch --partition multinode   --ntasks=640  --time=02:15:00 $1  >> jobIdsMultinodeFull.txt
+sbatch --partition multinode   --ntasks=1280  --time=02:35:00 $1  >> jobIdsMultinodeFull.txt
+#sbatch --partition multinode   --ntasks=2560  --time=01:35:00 $1  >> jobIdsMultinodeFull.txt
+#
+
+
+#sbatch --partition multinode   --ntasks=320  --time=00:35:00 $1  >> jobIdsMultinodeFull.txt
+#sbatch --partition multinode   --ntasks=500  --time=00:15:00 $1  >> jobIdsMultinodeFull.txt
+#sbatch --partition multinode   --ntasks=1000  --time=00:15:00 $1  >> jobIdsMultinodeFull.txt
