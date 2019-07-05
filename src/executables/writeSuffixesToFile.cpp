fa8c1b82bf9decc81211a51707c895a60d1b4d9f
Matthias Schimek <matthias_schimek@gmx.de>
2019-07-05 11:23:43 +0200

    do not reserve vector for all suffixes

--- src/executables/writeSuffixesToFile.cpp
+++ src/executables/writeSuffixesToFile.cpp
@@ -9,10 +9,33 @@
 
 #include <tlx/cmdline_parser.hpp>
 
+namespace makeSuffixes {
+std::vector<unsigned char> readFile(const std::string& path) {
+        using dss_schimek::RawStringsLines;
+        using namespace dss_schimek;
+        using namespace dss_schimek::mpi;
 
+        RawStringsLines data;
+        const size_t fileSize = getFileSize(path);
+        std::ifstream in(path);
+        std::vector<unsigned char>& rawStrings = data.rawStrings;
+        rawStrings.reserve(1.5 * fileSize);
+
+        std::string line;
+        data.lines = 0u;
+        while (std::getline(in, line)) {
+            ++data.lines;
+            for (unsigned char curChar : line)
+                rawStrings.push_back(curChar);
+        }
+        rawStrings.push_back(0);
+        in.close();
+        return rawStrings;
+    }
+
+}
 int main(std::int32_t argc, char const* argv[]) {
     using namespace dss_schimek;
-    using StringSet = dss_schimek::UCharLengthStringSet;
 
     dss_schimek::mpi::environment env;
     env.barrier();
@@ -28,10 +51,10 @@ int main(std::int32_t argc, char const* argv[]) {
         return -1;
     }
 
-    auto cont = dss_schimek::SuffixGenerator<StringSet>(path);
-    auto ss = cont.make_string_set();
-    for (auto& str : ss)
-      std::cout << ss.get_chars(str, 0) << std::endl;
+    const auto completeString = makeSuffixes::readFile(path);
+    for (uint64_t i = 0; i + 1< completeString.size(); ++i) {
+      std::cout << completeString.data() + i << std::endl;
+    }
     
     env.finalize();
 }
